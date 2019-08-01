31162ded4034a247a952a9d04e2f309cac9fef24
anil.rai01 <anil.rai01041998@gmail.com>
2019-08-01 23:08:28 +0545

    New Feature added: Moving Car and some new Tracks

--- main.cpp
+++ main.cpp
@@ -8,23 +8,32 @@ int main() {
 	RenderWindow window(VideoMode(1920, 1080), "Drift Race", sf::Style::Close | sf::Style::Titlebar);
 	RectangleShape player(Vector2f(70, 120));
 
-	Texture tx1, playerText;
-	tx1.loadFromFile("images/bgRet.png");
+	Texture bg, playerText;
+	bg.loadFromFile("images/newFbg.png");
+	bg.setSmooth(true);
+	
+
 	playerText.loadFromFile("images/Car.png");
 	playerText.setSmooth(true);
 
-	//Setting Player
+	//Setting Player Car
+	//player.setOrigin(Vector2f(0, 0));
 	player.setTexture(&playerText);
 
-	Sprite sp1;
-	sp1.setTexture(tx1);
-	sp1.setPosition(Vector2f(400, 0));
+	//Setting up a sprite
+	Sprite mainBG, mainBG2, trackSp;
+	mainBG.setTexture(bg);
+	mainBG2.setTexture(bg);
 
-	float x = 750, y = 900;
+	float x = 705, y = 900;
 	float maxSpeed = 2;
 	float speed = 0;
 	float acc = 2;
+	float startX = 0, startY = 0;
+	float BackgroundY1 = 0;
+	float BackgroundY2 = -1080;
 
+	//Loading Window
 	while (window.isOpen()) {
 		Event evnt;
 
@@ -38,14 +47,17 @@ int main() {
 
 		//bool Up = 0, Right = 0, Down = 0, Left = 0;
 		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+
 			if (player.getPosition().y >= 0 + acc) {
 				x = x;
 				y -= acc;
+				startY = 2.2f;
 			}
 			
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Right)) {
-			if ( player.getPosition().x <= 1140 - acc) {
+			//cout << player.getPosition().x;
+			if ( player.getPosition().x < 1270 - acc) {
 				x += acc;
 				y = y;
 			}
@@ -58,20 +70,35 @@ int main() {
 			}
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Left)) {
-			if (player.getPosition().x >= 750 + acc) {
+			//cout << player.getPosition().x;
+			if (player.getPosition().x >= 705 + acc) {
 				x -= acc;
 				y = y;
 			}
 		}
 
 		//car movement
-
+		//Creat scrolling background
+		mainBG.setPosition(Vector2f(320.0f, BackgroundY1));
+		mainBG2.setPosition(Vector2f(320.0f, BackgroundY2));
+		if (BackgroundY2 > 0)
+		{
+			BackgroundY1 = 0;
+			BackgroundY2 = BackgroundY1 - 1080;
+		}
+		BackgroundY1 += 1.0;
+		BackgroundY2 += 1.0;
 		
 
+		//Clearing and creating windows
 		window.clear();
-		window.draw(sp1);
+
+		window.draw(mainBG);
+		window.draw(mainBG2);
+		
 		player.setPosition(Vector2f(x, y));
 		window.draw(player);
+		
 		window.display();
 	}
 }
